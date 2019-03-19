8a1c5e78259cafe516baac7ead072fd28e939fb5
leoren0971 <872106608@qq.com>
2019-03-20 00:03:09 +0800

    ...

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -7,6 +7,7 @@ const defaultState = fromJS({
 
 export default (state = defaultState, action) => {
   if (action.type === actionTypes.SEARCH_FOCUS) {
+    // immutable 对象的set方法，会结合之前immutable对象的值，和设置的值，返回一个全新的对象
     return state.set('isFoucus', true);
   }
   if (action.type === actionTypes.SEARCH_BLUR) {
